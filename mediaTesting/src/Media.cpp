0d5761001dbd56916d96ac27d572c0ce7765d0c7
tterb <brettjpeterson@gmail.com>
2013-06-07 17:42:40 -0400

    add mediaTesting app to repo

---
+++ mediaTesting/src/Media.cpp
@@ -0,0 +1,78 @@
+//
+//  Media.cpp
+//  RFIDBook
+//
+//  Created by Brett Peterson on 6/7/13.
+//
+//
+
+#include "Media.h"
+
+Media::Media(){}
+Media::~Media(){}
+
+// Image only
+void Media::setup(string imgFile, float _x, float _y){
+    
+    setPosition(_x, _y);
+    img.setup(imgFile);
+    
+    hasVid = false;
+}
+
+// Image and video
+void Media::setup(string imgFile, string vidFile, float _x, float _y){
+
+    setPosition(_x, _y);
+    img.setup(imgFile);
+    
+    hasVid = true;
+    vidState = 0;
+    vid.loadMovie(vidFile);
+
+}
+
+void Media::setPosition(float _x, float _y){
+    x = _x;
+    y = _y;
+}
+
+
+void Media::tap(){
+    
+     //TODO check mediaState
+    
+    if (hasVid) {
+        if (vidState == 0) {
+            vidState = 1;
+            vid.play();
+        } else {
+            vidState = 0;
+            vid.stop();
+        }
+    }
+    
+}
+
+void Media::update(){
+
+    //TODO check mediaState to see what to do
+    
+    img.update();
+    
+
+    
+}
+
+void Media::draw(){
+    
+    //TODO check mediaState
+    
+    if (vidState == 0){
+        img.draw(x,y);
+    }
+    else {
+        vid.draw(x, y);
+    }
+    
+}
