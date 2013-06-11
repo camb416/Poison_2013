74d16f0c79bb26138de6bbf8a23cc5c09816ac12
Cameron Browning <camb416@gmail.com>
2013-06-11 15:59:55 -0400

    commenting and cleaning LanguageModel

--- RFIDBook/src/LanguageModel.cpp
+++ RFIDBook/src/LanguageModel.cpp
@@ -9,30 +9,29 @@
 #include "LanguageModel.h"
 
 LanguageModel::LanguageModel(string _xmlfile){
+    
     currentLanguageIndex = 0;
+    
+    // TODO: Add error correction on loading this file
     xml.loadFile(_xmlfile);
-    string xmlToPrint;
-    xml.copyXmlToString(xmlToPrint);
-    cout << "PRINTING XML::: " << xmlToPrint;
-    xml.pushTag("languages");
+    
+    
     // load the xml, make some Language structs
     numLanguages = xml.getNumTags("language");
     
-    
+
+    // parse the file, build a vector of structs.
+    xml.pushTag("languages");
     for(int i=0;i<numLanguages;i++){
         Language lang;
         lang.ident = xml.getAttribute("language", "id", "",i);
         lang.name = xml.getAttribute("language","name","",i);
         int howManyAttributes = xml.getNumAttributes("language",i);
-        //cout << howManyAttributes << ". ident: " << lang.ident << endl;
         languages.push_back(lang);
-        
     }
+    xml.popTag();
     
-   // cout << ""; // for breakpoint debugging
-
-                                    xml.popTag();
-    
+    // write a report to the console
     report();
 }
 
