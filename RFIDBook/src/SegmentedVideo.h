a6e53814a5f5313b405d6b2211add1d571bcf449
tterb <brettjpeterson@gmail.com>
2013-08-14 13:15:08 -0400

    add default value to show button bool

--- RFIDBook/src/SegmentedVideo.h
+++ RFIDBook/src/SegmentedVideo.h
@@ -33,7 +33,7 @@ class SegmentedVideo : public ofFadeVideo{
         void update();
         //void draw(float x, float y);
         void touch();
-        bool showButton;
+        bool showButton = false;
     
         //ofVideoPlayer player;
         //string animFile = "belladona.mov";
