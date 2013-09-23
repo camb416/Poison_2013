37ac2e3a2918bfc5d67ad7454413227784aa8a32
Cameron Browning <camb416@gmail.com>
2013-09-23 13:40:22 -0400

    Adding the new ofxQuad example to the base dir

---
+++ quadtest/src/ofxQuadCrop.h
@@ -0,0 +1,25 @@
+//
+//  ofxQuadCrop.h
+//  quadtest
+//
+//  Created by Cameron Browning on 9/18/13.
+//
+//
+
+#ifndef __quadtest__ofxQuadCrop__
+#define __quadtest__ofxQuadCrop__
+
+#include "ofxQuad.h"
+#include <iostream>
+
+class ofxQuadCrop : public ofxQuad{
+    
+    
+public:
+    void draw(int _screenW, int _screenH);
+    void draw();
+        
+
+};
+
+#endif /* defined(__quadtest__ofxQuadCrop__) */
